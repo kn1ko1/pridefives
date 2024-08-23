219e265134d2bba98c2eeaee702c12df7cceeb24
knikoi <niquaye@gmail.com>
2024-08-23 21:27:52 +0100

    Removed comments

--- main.go
+++ main.go
@@ -15,31 +15,3 @@ func main() {
 		fmt.Println("HTTP status 500 - Internal Server Errors")
 	}
 }
-
-
-// func formHandler(w http.ResponseWriter, r *http.Request) {
-// 	if r.URL.Path != "/" {
-// 		showError(w, "404 Page not found.", 404)
-// 		return
-// 	}
-
-// 	switch r.Method {
-// 	case "GET":
-// 		http.ServeFile(w, r, "index.html")
-// 	case "POST":
-// 		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
-// 		if err := r.ParseForm(); err != nil {
-// 			fmt.Print("HTTP status 500 - Internal Server Errors", err)
-// 			return
-// 		}
-// 	}
-// }
-
-// func showError(w http.ResponseWriter, message string, statusCode int) {
-// 	t, err := template.ParseFiles("template/errors.html")
-// 	if err == nil {
-// 		w.WriteHeader(statusCode)
-// 		t.Execute(w, message)
-// 		return
-// 	}
-// }
